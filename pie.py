9f9b4853d1635ac157df7d8a800beb3fc31e72dd
priyatam <priyatam@gmail.com>
2013-07-11 16:58:22 -0400

    added frozen flask build step

--- pie.py
+++ pie.py
@@ -1,9 +1,24 @@
-from flask import Flask
+import sys
+
+from flask import Flask, render_template
+from flask_flatpages import FlatPages
+from flask_frozen import Freezer
+
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+pages = FlatPages(app)
+freezer = Freezer(app)
 
 @app.route("/")
 def index():
     return "Hello Frozen Pie!"
 
-if __name__ == "__main__":
-    app.run(port=8000)
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.run(host='0.0.0.0', port=3000)
