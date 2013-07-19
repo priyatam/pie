67e52018a906d16ee5b65fa031fcdb3fb5a97387
priyatam <priyatam@gmail.com>
2013-07-18 22:57:45 -0400

    minor pep comments fix

--- bake.py
+++ bake.py
@@ -10,7 +10,6 @@ Generate index.html with included CSS
 
 """
 
-from __future__ import with_statement
 import os, time
 import yaml, markdown as md, pystache
 import json
@@ -37,7 +36,6 @@ class Config(object):
         with open(filename, "r") as filename_fin:
             return filename_fin.read()
 
-
     def style_sheet(self):
         css_file = self.yaml_data.get("theme")
         filename = "static/" + css_file
@@ -51,25 +49,26 @@ class Config(object):
             return filename_fin.read()
 
 
-def Post(front_matter, raw_content, name, ctime, mtime):
-        data = {
-            "body" : raw_content,
-            "name" : name,
-            "ctime" : ctime,
-            "mtime" : mtime
-            }
-        data.update(front_matter)
-        return data
+def Post(config, raw_content, name, ctime, mtime):
+    data = {
+        "body" : raw_content,
+        "name" : name,
+        "ctime" : ctime,
+        "mtime" : mtime
+        }
+    data.update(config)
+    return data
 
 
 def read_config():
     with open("config.yaml", "r") as content_file:
         text = content_file.read()
         return yaml.load(text)
+    
 
 
 def prepare_crust():
-    ''' '''
+    """ Prepares all Posts """
     global config
     global posts
     config_data = read_config()
@@ -86,14 +85,13 @@ def prepare_crust():
             shake_pan(post)
             posts.append(post)
 
-
 def __read(port):
-    ''' Splits file into a tuple of YAML and Markdown '''
+    """ Splits file into a tuple of YAML and Markdown """
     parts = port.read().split('\n---\n')
     if len(parts) == 1:
-        return (parts[0], {})
+        return parts[0], {}
     else:
-        return (parts[1], yaml.load(parts[0]))
+        return parts[1], yaml.load(parts[0])
 
 
 def markstache(post):
@@ -105,14 +103,15 @@ def markstache(post):
 
 
 def shake_pan(post):
-    ''' Export Crust to a Pan as {{ body }}.
-        A Pan is a template in Markdown'''
+    """Export Crust to a Pan as {{ body }}.
+        A Pan is a template in Markdown"""
     return markstache(post)
 
 
 def bake(style_sheet, script):
-    ''' Wrap the final page in Html5, CSS, JS, POSTS '''
-    print pystache.render(config.index_template(), { "style_sheet" : style_sheet, "script" : script, "json_data" : json.dumps(posts), "relative_path": config.relative_path() })
+    """ Wrap the final page in Html5, CSS, JS, POSTS """
+    print pystache.render(config.index_template(), {"style_sheet": style_sheet, "script": script, "json_data": json.dumps(posts), "relative_path": config.relative_path()})
+
 ## Main ##
 
 def main():
