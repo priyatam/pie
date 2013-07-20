414417a5ba0f35c880164923f0454064eae7ab73
priyatam <priyatam@gmail.com>
2013-07-20 00:26:04 -0400

    refactor, bake

--- bake.py
+++ bake.py
@@ -5,8 +5,8 @@ python bake.py > index.html
 
 Algo:
 Read config.yaml
-For each post, generate complete html by applying templates
-Generate index.html with included CSS
+For each post, generate complete html by applying Mustache templates
+Generate index.html with included CSS, JS
 
 """
 
@@ -14,90 +14,92 @@ import os, time
 import yaml, markdown as md, pystache
 import json
 
-
-def markstache(template, post):
-    """Expands Mustache templates from local YAML data and renders Markdown as HTML"""
-    raw_md = md.markdown(post['body'])
-    return pystache.render(template, {"body": raw_md})
-
-
 def load_config():
     """Loads configuration from config.yaml"""
     with open("config.yaml", "r") as fin:
         return yaml.load(fin.read())
 
 
+def load_posts(config):
+    """Creates a dictionary of Post meta data, including body as 'raw content'"""
+    posts = []
+    for fname in os.listdir("posts"):
+        yaml_data, md_data = read_markdown('posts', fname)
+        fname = 'posts' + os.sep + fname
+        post = {
+            "name": fname,
+            "body": md_data, # raw, unprocessed md
+            "created_date": time.ctime(os.path.getmtime(fname)),
+            "modified_date": time.ctime(os.path.getctime(fname))
+        }
+        post.update(yaml_data)  # Merge Yaml data
+        posts.append(post)
+
+    return posts
+
+
 def load_layout(config):
     """Reads templates, scripts, styles and stores them as dictionaries"""
-    
     content = {}
     # Group these by type in config
-    templates = dict(config, 'templates')
-    styles = dict(config, 'styles')
-    scripts = dict(config, 'scripts')
+    templates = rawcontent_by_fname(config, 'templates')
+    styles = rawcontent_by_fname(config, 'styles')
+    scripts = rawcontent_by_fname(config, 'scripts')
     # Merge them
     content.update(templates)
     content.update(styles)
     content.update(scripts)
-     
+
     return content
 
 
-def load_posts(config):
-    """Creates a dictionary of Post meta data, including body as 'raw content'"""
-    
-    posts = []    
-    for filename in os.listdir("posts"):
-        yaml_data, md_data = read_markdown('posts', filename)
-        filename = 'posts' + os.sep + filename
-        post = {
-            "name": filename,
-            "body": md_data, # raw, unprocessed md
-            "created_date": time.ctime(os.path.getmtime(filename)),
-            "modified_date": time.ctime(os.path.getctime(filename))
-        }     
-        post.update(yaml_data)  # Merge Yaml data
-        posts.append(post)        
-        
-    return posts
-    
-    
-def dict(config, key):
-    """Creates a dictionary of key->file (raw content)"""
-    return {filename: read(key, filename) for filename in config[key]}
+def rawcontent_by_fname(config, key):
+    """Creates a dictionary of fname->raw_content, where raw_content is config(key)"""
+    return {fname: read(key, fname) for fname in config[key]}
 
 
-def read(subdir, filename):
-    """Reads subdir/<filename> as raw content"""
-    with open(subdir + os.sep + filename, "r") as fin:
+def read(subdir, fname):
+    """Reads subdir/<fname> as raw content"""
+    with open(subdir + os.sep + fname, "r") as fin:
         return fin.read()
 
 
-def read_markdown(subdir, filename):
+def read_markdown(subdir, fname):
     """Splits Markdown file into a tuple of YAML and content"""
-    with open(subdir + os.sep + filename, "r") as fin:
+    with open(subdir + os.sep + fname, "r") as fin:
         yaml_and_md = fin.read().split('\n---\n')
         if len(yaml_and_md) == 1:
             return {}, yaml_and_md[0]
         else:
             return yaml.load(yaml_and_md[0]), yaml_and_md[1]
 
-def make_crust(content, posts):
-    """Parse Markdown, YAML, Mustache"""
+
+def markstache(post, template):
+    """Expands Mustache templates from local YAML data and renders Markdown as HTML"""
+    raw_md = md.markdown(post['body'])
+    return pystache.render(template, {"body": raw_md})
+
+
+def bake(config, content, posts):
+    """ Parse everything. Wrap results in a final page in Html5, CSS, JS, POSTS """
+    # Stache
     for post in posts:
-        post['html'] = markstache(post, content[post['template']]) 
-       
+        post['html'] = markstache(post, content[post['template']])
     
+    # Merge into one
+    output = pystache.render(content['index.mustache'],
+                             {"style_sheet": content['index.css'], "script": content['index.js'],
+                              "json_data": json.dumps(posts), "relative_path": config['relative_path']})
+    print output
+
+
 def main():
     """Let's cook an Apple Pie"""
-
     config = load_config()
     content = load_layout(config)
     posts = load_posts(config)
-    
-    make_crust(content, posts)
+    bake(config, content, posts)
 
-    
 
 if __name__ == '__main__':
     main()
--- bake_tests.py
+++ bake_tests.py
@@ -14,6 +14,7 @@ class BakeTests(unittest.TestCase):
         self.config = {'templates': ['post.mustache', 'index.mustache'], 'scripts': ['index.js'],
                        'styles': ['index.css'], 'relative_path': ['/index.html']}
 
+    
     def test_load_config(self):
         config = bake.load_config()
         self.assertEqual(types.DictType, type(config))
@@ -23,23 +24,27 @@ class BakeTests(unittest.TestCase):
         self.assertEqual(1, len(config['scripts']))
         self.assertEqual('/index.html', config['relative_path'])
 
-    def test_load_content(self):
+
+    def test_load_layout(self):
         content = bake.load_layout(self.config)
         self.assertEqual(types.DictType, type(content))
         for k, v in content.items():
             self.assertIsNotNone(k in self.config.values())
             self.assertIsNotNone(v)
 
+
     def test_read_markdown(self):
         yaml, post = bake.read_markdown('posts', 'page.md')
         self.assertEqual(5, len(yaml))
         self.assertIsNotNone(post)
 
+   
     def test_load_posts(self):
         posts = bake.load_posts(self.config)
         self.assertEqual(2, len(posts))
         self.assertIsNotNone(posts[0]['body'])
 
+  
     def test_markstache(self):
         post = {'body': """#What's the point of another static generator ?  
                             It's a *single file* static generator with all the bells and whistles.  
@@ -50,8 +55,11 @@ class BakeTests(unittest.TestCase):
                             {{{ body }}}
                         </div>                            
         """
-        stache = bake.markstache(template, post)
+        stache = bake.markstache(post, template)
         self.assertIsNotNone(stache)
-
+ 
+    def bake(self):
+        pass #TODO
+        
 if __name__ == '__main__':
     unittest.main()
