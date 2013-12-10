c7e8fc766e447f7a9885f87b62948b86e8d47fcb
priyatam <priyatam@gmail.com>
2013-12-10 14:07:44 -0800

    moved demo into 'site-with-blog'

--- examples/recipe/lambdas/default.py
+++
@@ -1,8 +0,0 @@
-import pystache
-
-def hello_world(text):
-    return "Hello %s from Frozen Pie" % text
-
-def all_posts(text):
-    posts = [ content for content in contents if content["_type"] == "blog" ]
-    return pystache.render(text, { "posts": posts })
