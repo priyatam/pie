e66614173a9b333e41c8e52472e468c5bb1258e9
Sreeharsha Mudivarti <msreeharsha@gmail.com>
2014-02-04 04:35:34 +0530

    pypi compatiable

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 
 setup(
     name='frozen-pie',
@@ -13,5 +17,22 @@ setup(
     license='Apache 2.0 License',
     author='Priyatam Mudivarti',
     author_email='priyatam@gmail.com',
-    description='Frozen Pie: a Python single-page static site generator'
+    description='Frozen Pie: a Python single-page static site generator',
+    install_requires=required,
+    classifiers=[
+            'Environment :: Console',
+            'Intended Audience :: End Users/Desktop',
+            'Intended Audience :: Developers',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: Apache License',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: Unix',
+            'Operating System :: POSIX',
+            'Programming Language :: Python',
+            'Topic :: Software Development',
+            'Topic :: Software Development :: Build Tools',
+            'Topic :: Software Development :: Code Generators',
+            'Topic :: Internet',
+            'Topic :: Internet :: WWW/HTTP :: Site Management',
+      ],
 )
